4ee3f09364f6ae5a16246b359e2f3c9c3edd0423
shahmalav <malavpshah19@gmail.com>
2016-06-23 20:02:31 -0700

    Initialize members of Paddle class

--- app/src/main/java/com/shahmalav/androidprojects/brickgame/Paddle.java
+++ app/src/main/java/com/shahmalav/androidprojects/brickgame/Paddle.java
@@ -1,8 +1,38 @@
 package com.shahmalav.androidprojects.brickgame;
 
+import android.graphics.RectF;
+
 /**
  * Created by shahm on 6/23/2016.
  */
 public class Paddle {
+    private RectF paddle;
+    private float width;
+    private float height;
+
+    private float x;
+    private float y;
+
+    private float speed;
+
+    public Paddle(int sx, int sy){
+        x=sx/2;
+        y=sy - 10;
+        height = 20;
+        width = 120;
+        speed = 200;
+        paddle = new RectF(x, y, x+width, y+height);
+    }
+
+    public RectF getPaddle(){
+        return paddle;
+    }
+
+
+    public void update(){
+        //TODO : update paddle x,y
+    }
+
+
 
 }
