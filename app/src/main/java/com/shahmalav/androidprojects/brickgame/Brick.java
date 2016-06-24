88beab4cecb1fdb010b1abff577de29991e80a01
shahmalav <malavpshah19@gmail.com>
2016-06-23 20:32:01 -0700

    Initialize members of Brick class

--- app/src/main/java/com/shahmalav/androidprojects/brickgame/Brick.java
+++ app/src/main/java/com/shahmalav/androidprojects/brickgame/Brick.java
@@ -1,7 +1,43 @@
 package com.shahmalav.androidprojects.brickgame;
 
+import android.graphics.RectF;
+
 /**
  * Created by shahm on 6/23/2016.
  */
 public class Brick {
+    private RectF brick;
+    private static final int PADDING = 1;
+    private boolean isVisible;
+    private int width;
+    private int height;
+    private int left;
+    private int top;
+    private int right;
+    private int bottom;
+
+    public Brick(int r, int c, int sx, int sy){
+        width = sx/8;
+        height = sy/10;
+
+        left = c*width+ PADDING;
+        top = r*height+ PADDING;
+        right = c*width+width-PADDING;
+        bottom = r*height+height-PADDING;
+
+        brick = new RectF(left, top, right, bottom);
+        isVisible = true;
+    }
+
+    public RectF getBrick(){
+        return brick;
+    }
+
+    public void destroy(){
+        isVisible = false;
+    }
+
+    public boolean isVisible(){
+        return isVisible;
+    }
 }
